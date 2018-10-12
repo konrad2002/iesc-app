87a26ff651880319d1ab3e49baf7b9fa15ebd972
konrad2002 <konrad@schwimmteamerzgebirge.de>
2018-10-12 21:30:15 +0200

    first commit

---
+++ IESC/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace IESC
+{
+    partial class main
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.website = new System.Windows.Forms.WebBrowser();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn_web_refresh = new System.Windows.Forms.Button();
+            this.btn_web_back = new System.Windows.Forms.Button();
+            this.btn_open_website = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60.85741F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 39.14259F));
+            this.tableLayoutPanel1.Controls.Add(this.btn_close, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.72046F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 539F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(805, 564);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btn_close
+            // 
+            this.btn_close.BackColor = System.Drawing.Color.Red;
+            this.btn_close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_close.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btn_close.ForeColor = System.Drawing.SystemColors.Window;
+            this.btn_close.Location = new System.Drawing.Point(777, 0);
+            this.btn_close.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(28, 25);
+            this.btn_close.TabIndex = 3;
+            this.btn_close.Text = "X";
+            this.btn_close.UseVisualStyleBackColor = false;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 66.16541F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.83459F));
+            this.tableLayoutPanel2.Controls.Add(this.tabControl1, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(2, 27);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 39.09884F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60.90116F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(485, 535);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(2, 2);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(316, 203);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Size = new System.Drawing.Size(308, 177);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Size = new System.Drawing.Size(308, 177);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.website, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel4, 0, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(491, 27);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 91.13372F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.866279F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(312, 535);
+            this.tableLayoutPanel3.TabIndex = 2;
+            // 
+            // website
+            // 
+            this.website.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.website.Location = new System.Drawing.Point(2, 2);
+            this.website.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.website.MinimumSize = new System.Drawing.Size(15, 16);
+            this.website.Name = "website";
+            this.website.ScriptErrorsSuppressed = true;
+            this.website.Size = new System.Drawing.Size(308, 483);
+            this.website.TabIndex = 0;
+            this.website.Url = new System.Uri("http://new.erzgebirgsschwimmcup.de", System.UriKind.Absolute);
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 3;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.23077F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.76923F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 144F));
+            this.tableLayoutPanel4.Controls.Add(this.btn_web_refresh, 1, 0);
+            this.tableLayoutPanel4.Controls.Add(this.btn_web_back, 0, 0);
+            this.tableLayoutPanel4.Controls.Add(this.btn_open_website, 2, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(2, 489);
+            this.tableLayoutPanel4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(308, 44);
+            this.tableLayoutPanel4.TabIndex = 1;
+            // 
+            // btn_web_refresh
+            // 
+            this.btn_web_refresh.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_web_refresh.Location = new System.Drawing.Point(82, 2);
+            this.btn_web_refresh.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_web_refresh.Name = "btn_web_refresh";
+            this.btn_web_refresh.Size = new System.Drawing.Size(79, 40);
+            this.btn_web_refresh.TabIndex = 0;
+            this.btn_web_refresh.Text = "Neu laden";
+            this.btn_web_refresh.Click += new System.EventHandler(this.btn_web_refresh_Click);
+            // 
+            // btn_web_back
+            // 
+            this.btn_web_back.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_web_back.Location = new System.Drawing.Point(2, 2);
+            this.btn_web_back.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_web_back.Name = "btn_web_back";
+            this.btn_web_back.Size = new System.Drawing.Size(76, 40);
+            this.btn_web_back.TabIndex = 0;
+            this.btn_web_back.Text = "Zurück";
+            this.btn_web_back.UseVisualStyleBackColor = true;
+            this.btn_web_back.Click += new System.EventHandler(this.btn_web_back_Click);
+            // 
+            // btn_open_website
+            // 
+            this.btn_open_website.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_open_website.Location = new System.Drawing.Point(165, 2);
+            this.btn_open_website.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_open_website.Name = "btn_open_website";
+            this.btn_open_website.Size = new System.Drawing.Size(141, 40);
+            this.btn_open_website.TabIndex = 0;
+            this.btn_open_website.Text = "Website im Browser öffnen";
+            this.btn_open_website.UseVisualStyleBackColor = true;
+            this.btn_open_website.Click += new System.EventHandler(this.btn_open_website_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 564);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.Name = "main";
+            this.Text = "IESC";
+            this.Load += new System.EventHandler(this.main_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.Button btn_web_back;
+        private System.Windows.Forms.Button btn_open_website;
+        private System.Windows.Forms.Button btn_web_refresh;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.WebBrowser website;
+        private System.Windows.Forms.FontDialog fontDialog1;
+    }
+}
+
---
+++ IESC/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Ich hab ken plan wie github funktioniert
+
+namespace IESC
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_open_website_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://new.erzgebirgsschwimmcup.de");
+        }
+
+        private void btn_web_refresh_Click(object sender, EventArgs e)
+        {
+            website.Refresh();
+        }
+
+        private void btn_web_back_Click(object sender, EventArgs e)
+        {
+            website.GoBack();
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void website_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (IsHorizontalScrollbarPresent)
+            {
+                website.Size = new Size(website.Document.Body.ScrollRectangle.Width, website.Document.Body.ScrollRectangle.Height);
+                main.ActiveForm.Size = new Size(website.Document.Body.ScrollRectangle.Width, website.Document.Body.ScrollRectangle.Height);
+            }
+        }
+
+        public bool IsHorizontalScrollbarPresent
+        {
+            get
+            {
+                var widthofScrollableArea = website.Document.Body.ScrollRectangle.Width;
+                var widthofControl = website.Document.Window.Size.Width;
+
+                return widthofScrollableArea > widthofControl;
+            }
+        }
+
+        private void btn_font_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
+        }
+    }
+}
