a6b2de64406db61f153f099f42b0eb4dbb3f533f
konrad2002 <konrad@schwimmteamerzgebirge.de>
2018-10-13 13:31:49 +0200

    new button styles

--- IESC/Form1.Designer.cs
+++ IESC/Form1.Designer.cs
@@ -60,7 +60,7 @@
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.72046F));
@@ -73,6 +73,7 @@
             this.btn_close.BackColor = System.Drawing.Color.Red;
             this.btn_close.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_close.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btn_close.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btn_close.ForeColor = System.Drawing.SystemColors.Window;
             this.btn_close.Location = new System.Drawing.Point(777, 0);
             this.btn_close.Margin = new System.Windows.Forms.Padding(0);
@@ -91,7 +92,7 @@
             this.tableLayoutPanel2.Controls.Add(this.tabControl1, 0, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(2, 27);
-            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(2);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 2;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 39.09884F));
@@ -104,7 +105,7 @@
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Location = new System.Drawing.Point(2, 2);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(316, 203);
@@ -113,9 +114,9 @@
             // tabPage1
             // 
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
             this.tabPage1.Size = new System.Drawing.Size(308, 177);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
@@ -124,9 +125,9 @@
             // tabPage2
             // 
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2);
             this.tabPage2.Size = new System.Drawing.Size(308, 177);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
@@ -140,7 +141,7 @@
             this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel4, 0, 1);
             this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel3.Location = new System.Drawing.Point(491, 27);
-            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(2);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 2;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 91.13372F));
@@ -152,13 +153,13 @@
             // 
             this.website.Dock = System.Windows.Forms.DockStyle.Fill;
             this.website.Location = new System.Drawing.Point(2, 2);
-            this.website.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.website.Margin = new System.Windows.Forms.Padding(2);
             this.website.MinimumSize = new System.Drawing.Size(15, 16);
             this.website.Name = "website";
             this.website.ScriptErrorsSuppressed = true;
             this.website.Size = new System.Drawing.Size(308, 483);
             this.website.TabIndex = 0;
-            this.website.Url = new System.Uri("http://new.erzgebirgsschwimmcup.de", System.UriKind.Absolute);
+            this.website.Url = new System.Uri("http://www.erzgebirgsschwimmcup.de", System.UriKind.Absolute);
             // 
             // tableLayoutPanel4
             // 
@@ -171,7 +172,7 @@
             this.tableLayoutPanel4.Controls.Add(this.btn_open_website, 2, 0);
             this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel4.Location = new System.Drawing.Point(2, 489);
-            this.tableLayoutPanel4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel4.Margin = new System.Windows.Forms.Padding(2);
             this.tableLayoutPanel4.Name = "tableLayoutPanel4";
             this.tableLayoutPanel4.RowCount = 1;
             this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
@@ -181,8 +182,10 @@
             // btn_web_refresh
             // 
             this.btn_web_refresh.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_web_refresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_web_refresh.ForeColor = System.Drawing.Color.White;
             this.btn_web_refresh.Location = new System.Drawing.Point(82, 2);
-            this.btn_web_refresh.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_web_refresh.Margin = new System.Windows.Forms.Padding(2);
             this.btn_web_refresh.Name = "btn_web_refresh";
             this.btn_web_refresh.Size = new System.Drawing.Size(79, 40);
             this.btn_web_refresh.TabIndex = 0;
@@ -192,8 +195,10 @@
             // btn_web_back
             // 
             this.btn_web_back.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_web_back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_web_back.ForeColor = System.Drawing.Color.White;
             this.btn_web_back.Location = new System.Drawing.Point(2, 2);
-            this.btn_web_back.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_web_back.Margin = new System.Windows.Forms.Padding(2);
             this.btn_web_back.Name = "btn_web_back";
             this.btn_web_back.Size = new System.Drawing.Size(76, 40);
             this.btn_web_back.TabIndex = 0;
@@ -204,8 +209,10 @@
             // btn_open_website
             // 
             this.btn_open_website.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_open_website.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_open_website.ForeColor = System.Drawing.Color.White;
             this.btn_open_website.Location = new System.Drawing.Point(165, 2);
-            this.btn_open_website.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_open_website.Margin = new System.Windows.Forms.Padding(2);
             this.btn_open_website.Name = "btn_open_website";
             this.btn_open_website.Size = new System.Drawing.Size(141, 40);
             this.btn_open_website.TabIndex = 0;
@@ -221,7 +228,7 @@
             this.Controls.Add(this.tableLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "main";
             this.Text = "IESC";
